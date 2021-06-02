8373f3845f15fe817c15cf103234c2e76987b48e
dev <dev@gmail>
2021-06-02 11:13:03 +0530

    stash completed

--- pdfEditByNit/src/com/app/bean/UserBean.java
+++ pdfEditByNit/src/com/app/bean/UserBean.java
@@ -6,7 +6,20 @@ public class UserBean {
 	 System.out.println("again doing prec...123dfssss");
 
  }
+
  public void z() {
 	 System.out.println("dsf");
+ }
+
+ public void m() {
+	 System.out.println("dsf");
+ }
+ public void m2() {
+	 
+ }
+ 
+ public void v() {
+	 System.out.println("dfs");
+
  }
 }
