a9dd97093295f29b620cc19ea1ee1249dd05fc41
dev <dev@gmail>
2021-06-02 09:33:19 +0530

    main method has added just to print date.

--- pdfEditByNit/src/com/app/test/Prec.java
+++ pdfEditByNit/src/com/app/test/Prec.java
@@ -5,5 +5,10 @@ import java.util.Date;
 public class Prec {
    public void get() {
 	   Date d =new Date();
+	   System.out.println(d);
    }
+   public static void main(String[] args) {
+	Prec p = new Prec();
+	p.get();
+}
 }
